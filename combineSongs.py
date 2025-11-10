c5808269ed076c2c6e7606b0cfd4684046c49c48
ishwaryanam <inamburu@calpoly.edu>
2025-11-10 10:57:54 -0800

    setting up

---
+++ combineSongs.py
@@ -0,0 +1,25 @@
+
+import sys
+'''
+the Plan:
+
+Take 2 files from command line, file1 #secs file2 #secs 
+
+	- by default take last and first seconds 
+ 
+ '''
+ 
+
+
+def main():
+    file1 = sys.argv[1] #28 seconds
+    file1_seconds = sys.argv[2]
+    file2 = sys.argv[3] #23 seconds
+    file2_seconds = sys.argv[4]
+    
+    print(file1)
+    print(file1_seconds)
+    print(file2)
+    print(file2_seconds)
+
+main()
